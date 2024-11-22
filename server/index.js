fcbb5c2efb2a568a747533232ec1464a2449347f
Shivalika Raj <shivalikaraj0099@gmail.com>
2024-11-22 19:35:56 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -13,25 +13,12 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [
-    "http://localhost:5173", // Local development
-    "https://job-finder-sr.netlify.app" // Hosted frontend on Netlify
-  ];
+const corsOptions = {
+    origin: '"https://job-finder-sr.netlify.app" ',
+    credentials: true
+}
 
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            // Allow requests with no origin (like mobile apps or curl)
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-        credentials: true, // If cookies or authentication tokens are required
-    })
-);
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
@@ -41,7 +28,7 @@ const PORT = process.env.PORT || 3000;
 // API's
 app.use("/api/user", userRoute);
 app.use("/api/company", companyRoute)
-app.use("/api/job", jobRoute)
+app.use("/api/job",jobRoute)
 app.use("/api/application", applicationRoute)
 
 connectDB()
